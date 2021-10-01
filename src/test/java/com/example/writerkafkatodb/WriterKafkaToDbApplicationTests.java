d6f64e6766b7cb6aec888cb9c7ee1a4146499171
Stanislav Melekhin <darkprado@yandex.ru>
2021-10-01 09:19:55 +0300

    init commit

---
+++ src/test/java/com/example/writerkafkatodb/WriterKafkaToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.writerkafkatodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterKafkaToDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
